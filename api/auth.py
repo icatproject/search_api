ae9261c676e12a4f75260958dbe08c60deec9cc4
Alex Kemp <alexander.kemp@stfc.ac.uk>
2024-11-12 16:28:44 +0000

    Added more logging

--- api/auth.py
+++ api/auth.py
@@ -4,6 +4,7 @@ from fastapi import HTTPException, Security
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
 from api.config import settings
+from api.logger import app_logger
 
 # Define the security scheme to expect an Authorization header
 security = HTTPBearer()
@@ -14,6 +15,7 @@ async def authenticate_and_decode(credentials: HTTPAuthorizationCredentials = Se
     token = credentials.credentials  # Extract the JWT from the Authorization header
     # Validate the token
     try:
+        app_logger.info(f"Trying to validate JWT at {settings.scigateway_auth}")
         response = requests.post(settings.scigateway_auth, json={"token": token})
         # Check if validation failed
         if response.status_code != 200:
@@ -30,10 +32,12 @@ async def authenticate_and_decode(credentials: HTTPAuthorizationCredentials = Se
 
     except HTTPException as e:
         # Re-raise HTTPException directly without modification
+        app_logger.error(f"HTTPException {e}")
         raise e
 
     except Exception as e:
         # Raise all other exceptions as 500 Internal Server Error
+        app_logger.error(f"Exception {e}")
         raise HTTPException(
             status_code=500,
             detail=f"Internal server error: {str(e)}"
